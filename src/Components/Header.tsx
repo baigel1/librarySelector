e745cf4b63fc66a59a012c65f89930123d3132f0
Daniel <dbaigel@yext.com>
2022-06-15 17:34:40 -0400

    changed logo and added descriptions for each library

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,10 +1,10 @@
-import yextImage from '../Images/yextLogo.png'
+import yextImage from '../Images/yextLogoWhite.png'
 
 const Header = () => {
     return (
-        <div className="flex justify-center items-center bg-rose-400 h-64 text-4xl">
+        <div className="flex justify-center items-center bg-black h-64 text-4xl">
             <img src={yextImage} alt="Yext logo" className="w-32"/>
-            <h1 className="mx-8">Answer the questions below to discover the right library for you!</h1>
+            <h1 className="mx-8 text-white">Answer the questions below to discover the right library for you!</h1>
         </div>
     )
 }
--- src/Components/Selector.tsx
+++ src/Components/Selector.tsx
@@ -6,6 +6,7 @@ const Selector = () => {
     const [questionNumber, setQuestionNumber] = useState(0)
     const [foundResult, setFoundResult] = useState("")
     const [documentationLink, setDocumentationLink] = useState("")
+    const [libraryDescription, setLibraryDescription] = useState("")
     const questions = [
         "Do you want to build a web-based experience?",
         "Do you want to use React?",
@@ -21,6 +22,7 @@ const Selector = () => {
                 if(answers[0] === 'False') {
                     setFoundResult("Answers API!") 
                     setDocumentationLink("https://hitchhikers.yext.com/docs/liveapis/answersapi/")
+                    setLibraryDescription("The Answers API is great if you are building a non-web application like a mobile app. It's a REST API used for querying data that contains different endpoints for Universal Search and Vertical Search.")
                 }  
                 break
             }
@@ -40,11 +42,15 @@ const Selector = () => {
                 if (answers[2] === 'True') {
                     setFoundResult(`Answers Headless!`)
                     setDocumentationLink("https://github.com/yext/answers-headless")
+                    setLibraryDescription("Headless is our Redux state management library that has React bindings (if you want to use React) but is also general enough to be used in any framework of your choice! (Vue and Angular bindings coming soon)!")
+
                 }
                 else if (answers[2] === 'False'){
                  
                     setFoundResult(`Answers Core!`)
                     setDocumentationLink("https://hitchhikers.yext.com/docs/answerscoresdk/")
+                    setLibraryDescription("Answers Core is a TypeScript library that’s used for interacting with the Answers API. It does not require access to the DOM so you can use it in either the browser or Node if you want to.")
+
                 }
                 else {
                     //we are skipping the question
@@ -58,10 +64,14 @@ const Selector = () => {
                     if (answers[3] === 'True') {
                         setFoundResult(`React Component Library!`)
                         setDocumentationLink("https://github.com/yext/answers-react-components")
+                        setLibraryDescription("The React Component Library is great if you are using React and want to get your experience up and running as quickly as possible with some pre-defined components. It contains components for your search bar, results, facets, and more!")
+
                     }
                     else {
                         setFoundResult(`React Headless!`)
                         setDocumentationLink("https://github.com/yext/answers-headless-react")
+                        setLibraryDescription("If you want to use React as well as a library that handles state management for you, but would rather have full control over each component's UI, then this library is perfect for you!")
+
                     }
                 }
                 break
@@ -86,16 +96,21 @@ const Selector = () => {
     return (
         <>
         <button onClick={startOver} className="my-4 opacity-80 bg-slate-400 black p-4 text-center rounded-md w-32 mx-4 font-bold text-black active:bg-slate-600 hover:bg-slate-500">Start Over?</button>
-        <div className="flex flex-col items-center my-16">
-            { foundResult ? <h1 className="text-7xl">{foundResult} Click <a href={documentationLink} className="text-cyan-800 font-bold">here</a> for documentation</h1> :
+        <div className="flex flex-col items-center my-16 text-center">
+            { foundResult ? 
+            <>
+                <h1 className="text-7xl">{foundResult} Click <a href={documentationLink} className="text-cyan-800 font-bold">here</a> for documentation</h1>
+                <p className= "text-xl my-8">{libraryDescription}</p> 
+            </>
+            :
             <>
             <Question question={questions[questionNumber]}/>
             <div className="flex space-between my-8">
                 <button className="opacity-80 bg-blue-500 white p-4 text-center rounded-md w-64 mx-4 font-bold text-white active:bg-blue-800 active:text-slate-500 hover:bg-blue-600" onClick={handleSelectAnswer} value="True">
-                TRUE
+                YES
                 </button>
                 <button className=" opacity-80 bg-red-500 white p-4 text-center rounded-md w-64 mx-4 font-bold text-white active:bg-red-800 active:text-slate-500 hover:bg-red-600" onClick={handleSelectAnswer} value="False">
-                FALSE
+                NO
                 </button>
             </div>
             </>
